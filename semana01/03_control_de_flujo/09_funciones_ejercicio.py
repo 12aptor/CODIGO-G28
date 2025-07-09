8b7c890cd1f963da585bd671852538a0dbc3d159
12aptor <paholo2054@gmail.com>
2025-07-08 22:18:43 -0500

    funciones ejercicio

---
+++ semana01/03_control_de_flujo/09_funciones_ejercicio.py
@@ -0,0 +1,25 @@
+""" Crear un generador de contraseñas seguras. El
+programa debe pedir la longitud de la contraseña"""
+
+
+import string
+import random
+import test
+
+print(__name__)
+
+def generar_contrasena(longitud):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    contrasena = ""
+    for i in range(longitud):
+        contrasena += random.choice(caracteres)
+    return contrasena
+    
+
+def main():
+    longitud = int(input("Ingrese la longitud de la contraseña: "))
+    contrasena = generar_contrasena(longitud)
+    print(f"Tu contraseña segura es: {contrasena}")
+
+if __name__ == "__main__":
+    main()
