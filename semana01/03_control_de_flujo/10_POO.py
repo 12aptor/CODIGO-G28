e5e4e463fc364b3bfcb44b55e6e9045ee47b111c
12aptor <paholo2054@gmail.com>
2025-07-09 19:31:01 -0500

    POO

--- semana01/03_control_de_flujo/10_POO.py
+++ semana01/03_control_de_flujo/10_POO.py
@@ -5,16 +5,27 @@ class Vehiculo:
         self.color = color
         self.marca = marca
         self.modelo = modelo
+        """ Atributos privados """
+        self.__privado = 0
 
     def encender(self):
         print(f"Encendiendo el vehiculo {self.marca}...")
+        print(self.__privado)
+        self.__reiniciar()
 
     def apagar(self):
         print(f"Apagando el vehiculo {self.marca}...")
 
+    """ Métodos privados (Encapsulamiento) """
+    def __reiniciar(self):
+        print(f"Reiniciando el vehículo {self.marca}...")
+
+""" Instanciación de las clases """
 vw = Vehiculo("rojo", "VW", "Golf")
 print(vw.marca)
 vw.encender()
+# print(vw.__privado)
+# vw.__reiniciar()
 
 honda = Vehiculo("azul", "Honda", "Civic")
 print(honda.marca)
