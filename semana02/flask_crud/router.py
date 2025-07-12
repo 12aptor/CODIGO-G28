86c5ad65ecb741ef37b8ba521701388d7c93f6da
12aptor <paholo2054@gmail.com>
2025-07-11 21:51:38 -0500

    crud

--- semana02/flask_crud/router.py
+++ semana02/flask_crud/router.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 from resources.user_resource import UserResource
 
 user_router = Blueprint('user_router', __name__, url_prefix='/api/user')
@@ -11,15 +11,17 @@ def listUsers():
 
 @user_router.post('/create') # /api/user/create
 def createUser():
+    json = request.get_json()
     user_resource = UserResource()
-    return user_resource.create()
+    return user_resource.create(json)
 
-@user_router.put('/update') # /api/user/update
-def updateUser():
+@user_router.put('/<int:id>/update') # /api/user/<int:id>/update
+def updateUser(id):
+    json = request.get_json()
     user_resource = UserResource()
-    return user_resource.update()
+    return user_resource.update(id, json)
 
-@user_router.delete('/delete') # /api/user/delete
-def deleteUser():
+@user_router.delete('/<int:id>/delete') # /api/user/<int:id>/delete
+def deleteUser(id):
     user_resource = UserResource()
-    return user_resource.delete()
+    return user_resource.delete(id)
